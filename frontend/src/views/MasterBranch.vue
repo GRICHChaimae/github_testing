d1687543da5734762f6d228f95c9c5989036cb61
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-10-07 16:06:13 +0100

    se

--- frontend/src/views/MasterBranch.vue
+++ frontend/src/views/MasterBranch.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <p>master branch from second branch merge</p>
-    <a href="#">link</a>
-    <button>ssss</button>
-    <h1>sssss</h1>
-    <span>sssssss</span>
-    <h2>slg</h2>
+    <p>second</p>
   </div>
 </template>
