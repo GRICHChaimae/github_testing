fc427296d940d2f1389f06a6066363f90c8d6803
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-10-07 15:57:45 +0100

    th

--- frontend/src/views/MasterBranch.vue
+++ frontend/src/views/MasterBranch.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <p>from third one</p>
-    <a href="#">link</a>
-    <button>ssss</button>
-    <h1>sssss</h1>
-    <span>sssssss</span>
-    <h2>slg</h2>
-    <a href="#">walo</a>
+    <p>third</p>
   </div>
 </template>
